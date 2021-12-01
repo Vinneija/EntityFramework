5a3521e4873aed0109433b244d071d6e0a3dfb80
vinneija <neija.vineta@gmail.com>
2021-12-01 21:39:45 +0200

    Add project files.

---
+++ EntityFramework/Context/UniversityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramework.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-9KVL4IOV\SQLEXPRESS01; Database=UniversityDB; Trusted_Connection=True;");
+
+    }
+}
---
+++ EntityFramework/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EntityFramework.Context;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Students>();
+            using (var context = new UniversityDbContext())
+            {
+                students = context.Students.ToList();
+            }
+            students.ForEach(x => Console.WriteLine(x.Name));
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
